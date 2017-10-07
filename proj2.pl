d2d32ade5a860313df7a203da41098e1466a9b5f
Daniel Vu <vuh2@student.unimelb.edu.au>
2017-10-07 17:28:32 +1100

    init proj2.pl

---
+++ proj2.pl
@@ -0,0 +1,7 @@
+% University of Melbourne, Semester 2 2017
+% COMP30020: Declarative Programming
+% Project 2: The Math Puzzels
+% File     : proj2.pl
+% Author   : Duy Vu - vuh2@student.unimelb.edu.au 
+% Purpose  : Return puzzale solution for each test run by proj2_test.pl
+
